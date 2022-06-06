0bd4aec81402c86cc7bd41ddccb569ea5a4daa19
stiln <stiln@LAPTOP-NUEU3GPQ>
2022-06-06 18:28:16 -0400

    initial commit

---
+++ src/test/java/runner/XYZRunnerTest.java
@@ -0,0 +1,21 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions (
+		       features = "src/test/resources/orangehrm/",
+		       glue = {"steps"},
+		       plugin = {"pretty", "html:target/cucmber-report.html"},
+		       monochrome = true 
+		
+		) 
+
+public class XYZRunnerTest {
+
+	
+}
---
+++ src/test/java/steps/XYZSmokeStep.java
@@ -0,0 +1,13 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+
+public class XYZSmokeStep {
+	
+	@Given("User navigates to login page")
+	public void user_navigates_to_login_page() {
+	   
+	}
+
+
+}
